59d76ae2f4f1c5a30e14b68d7747d7f4eea8a3c0
Sumit Kumar <sumitkumar1209@gmail.com>
2016-02-07 13:19:17 +0530

    apt-get update for mongo

--- mw-cw-cluster/mongo-install-ubuntu.sh
+++ mw-cw-cluster/mongo-install-ubuntu.sh
@@ -31,7 +31,7 @@ sudo -u ubuntu git clone https://$GIT_AUTH@github.com/minewhat/Server.git
 # mongo install
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
-
+sudo apt-get update --yes
 # Disable THP
 sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
 sudo echo never > /sys/kernel/mm/transparent_hugepage/defrag
