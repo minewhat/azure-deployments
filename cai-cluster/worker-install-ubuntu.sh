96d6bac2127ec4d1402c731a00b6701dd7e5ee40
Sumit Kumar <sumitkumar1209@gmail.com>
2016-05-30 12:56:42 +0530

    kibana enricher worker

--- cai-cluster/worker-install-ubuntu.sh
+++ cai-cluster/worker-install-ubuntu.sh
@@ -146,6 +146,19 @@ sudo -u ubuntu sh scripts/startworker.sh
 sudo service supervisord stop
 echo "
 
+[program:cai_kibanaEnricher]
+command=/usr/bin/python /home/ubuntu/minewhat/workers/workers/choiceai/kibanaEnricher.py
+user=ubuntu
+autostart=false
+stdout_logfile_maxbytes = 50MB
+stdout_logfile_backups = 1
+autorestart=true
+startsecs=10
+stopsignal=KILL
+logfile = /raid1/supervisorlogs/program:cai_kibanaEnricher.log
+logfile_maxbytes = 50MB
+logfile_backups=1
+
 [program:cai_exportWorker]
 command=/usr/bin/python /home/ubuntu/minewhat/workers/workers/exportWorker.py
 user=ubuntu
